7fa24289f757f0c8b15e07b81cb051298e910d7a
takeshi.miki <nextat_staff@adminnoMacBook-Pro.local>
2023-03-02 10:43:21 +0900

    Finish Creating Chirps

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+           Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+           Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+           Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+      });
+
+Route::resource('chirps', ChirpController::class)
+    ->only(['index', 'store'])
+    ->middleware(['auth', 'verified']);
+
+require __DIR__.'/auth.php';
